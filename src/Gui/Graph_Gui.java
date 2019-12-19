6049e5e9cb24fb3b576e8165bc0bca4148c94b7f
DorGetter <57187365+DorGetter@users.noreply.github.com>
2019-12-19 22:56:49 +0200

    GUI v1

--- src/Gui/Graph_Gui.java
+++ src/Gui/Graph_Gui.java
@@ -1,5 +1,188 @@
 package Gui;
 
-public class Graph_Gui {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.swing.JFrame;
+
+import dataStructure.DGraph;
+import dataStructure.edge_data;
+import dataStructure.node_data;
+import elements.NodeV;
+
+
+
+public class Graph_GUI extends JFrame implements ActionListener, MouseListener {
+
+	//holds the vertex points.\\ 
+	//ArrayList<NodeV> p_list = new ArrayList<NodeV>(); 
+	HashMap<Integer,node_data> vertex;
+
+	// contains all the edges by ID(src ver) and edge_data. 
+	HashMap<Integer, edge_data> edges;
+
+	private DGraph Graph;
+
+
+	public Graph_GUI(DGraph dg)
+	{
+		initGUI();
+		this.vertex	= (HashMap<Integer, node_data>)dg.getV();
+		this.Graph = dg;
+	}
+
+	private void initGUI() {
+		this.setSize(1000,1000);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//creating menu bar//
+		MenuBar menuBar	 = new MenuBar();
+		Menu menu_file	 = new Menu("file"); 
+		//adding the file section to the menu bar//
+		menuBar.add(menu_file);
+		this.setMenuBar(menuBar);
+
+		//creating a item in bar for short path
+		MenuItem Shortest_Path = new MenuItem("Short Path"); 
+		Shortest_Path.addActionListener(this);
+
+		//creating a item in bar for is connected
+		MenuItem is_connected = new MenuItem("connect check"); 
+		is_connected.addActionListener(this);
+
+		menu_file.add(is_connected);
+		menu_file.add(Shortest_Path);
+
+		//listen to the mouse\\
+		this.addMouseListener(this);
+	}
+
+
+	public void paint(Graphics g)
+	{
+		//call paint func\\
+		super.paint(g);
+
+		Set setMapKey = vertex.keySet();
+		Iterator hit = setMapKey.iterator();
+
+		while(hit.hasNext()) {
+			//creating the vertex\\ 
+			int v = (int) hit.next(); 
+			g.setColor(Color.BLUE);
+			g.fillOval(Graph.getNode(v).getLocation().ix() , Graph.getNode(v).getLocation().iy(), 10 , 10);	
+
+			//creating edges to the vertex\\ 
+			edges = (HashMap<Integer, edge_data>) Graph.getE(v);
+			if(edges == null) {continue;}
+
+
+
+
+			//go over the edges that come out of the specific vertex\\ 
+			Set setMapKey2 = edges.keySet();
+			Iterator hit2 = setMapKey.iterator();
+
+			while(hit2.hasNext()) {
+				g.setColor(Color.red);
+				int dest = (int) hit2.next();
+				//From\\
+				int x1 = Graph.getNode(v).getLocation().ix();
+				int y1 = Graph.getNode(v).getLocation().iy();
+				//To\\
+				int x2 = Graph.getNode(dest).getLocation().ix();
+				int y2 = Graph.getNode(dest).getLocation().iy();
+
+				//draw the line between the vertexes\\ 
+				g.drawLine(x1, y1,x2,y2);
+
+
+				//Draw the circle indicates the direction of the edge,
+				//by mark a oval in the 3/4 the line next to the dest vertex. 
+				g.drawOval(((x1*1)/4)+((x2*3)/4),((y1*1)/4)+((y2*3)/4) , 3, 3);
+
+				double w = Graph.getNode(v).getWeight();
+				g.setColor(Color.MAGENTA);
+				g.drawString(Double.toString(w),(x1+x2)/2,(y1+y2)/2);
+
+			}
+
+		}
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		String action = e.getActionCommand(); 
+
+		if(action.contentEquals("Short Path"))
+		{
+			//create integration to the method.
+		}
+		if(action.contentEquals("connect check"))
+		{
+			//create integration to the method.
+		}
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
